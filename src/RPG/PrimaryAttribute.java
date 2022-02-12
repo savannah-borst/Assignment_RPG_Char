29335436c4b0f988e8164217798ff7409d2f1d55
savannah-borst <savannah.borst@gmail.com>
2022-02-12 20:26:54 +0100

    Added PrimaryAttribute Class + fields + constructor

---
+++ src/RPG/PrimaryAttribute.java
@@ -0,0 +1,13 @@
+package RPG;
+
+public class PrimaryAttribute {
+    private int strength;
+    private int dexterity;
+    private int intelligence;
+
+    public PrimaryAttribute(int strength, int dexterity, int intelligence) {
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+}
