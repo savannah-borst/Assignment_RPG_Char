b407317b88e3ad9ce90ffd12d34090f81545c504
savannah-borst <savannah.borst@gmail.com>
2022-02-13 16:03:45 +0100

    Item class constructor

--- src/RPG/Item.java
+++ src/RPG/Item.java
@@ -4,4 +4,18 @@ public abstract class Item {
     private String name;
     private int requiredLevel;
     //slot
+
+    public Item(String name, int requiredLevel) {
+        this.name = name;
+        this.requiredLevel = requiredLevel;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public int getRequiredLevel() {
+        return requiredLevel;
+    }
 }
