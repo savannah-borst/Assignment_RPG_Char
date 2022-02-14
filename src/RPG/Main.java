47bc55077ec343f68f809bb28e72ac698d8d63bf
savannah-borst <savannah.borst@gmail.com>
2022-02-14 21:23:34 +0100

    added testing for equipment to main

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        Warrior thor = new Warrior("Thor");
+        Weapon mjolnir = new Weapon("Mjolnir", 1, "Weapon", Weapon.WeaponType.HAMMER,5, 4.5);
+        thor.equipWeapon(mjolnir);
+
+        System.out.println(thor.getCharEquipment());
+        System.out.println(thor.getCharEquipment());
     }
 }
