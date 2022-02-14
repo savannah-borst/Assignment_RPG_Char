6fd50be3d36d6f3f5868c069a3d5112b0f0a7867
savannah-borst <savannah.borst@gmail.com>
2022-02-14 16:41:03 +0100

    added default attribute testing for every RPG class. deleted the testing in Main

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -5,21 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Mage jael = new Mage("Jael");
-        Rogue savannah = new Rogue("Savannah");
-        Ranger charona = new Ranger("Charona");
-        Warrior mathijs = new Warrior("Mathijs");
-
-        System.out.println(jael.getName() + " " + jael.getLevel() + " " + jael.getBaseAttributes());
-        System.out.println(savannah.getName() + " " + savannah.getLevel() + " " + savannah.getBaseAttributes());
-        System.out.println(charona.getName() + " " + charona.getLevel() + " " + charona.getBaseAttributes());
-        System.out.println(mathijs.getName() + " " + mathijs.getLevel() + " " + mathijs.getBaseAttributes());
-
-        jael.levelup();
-        savannah.levelup();
-        charona.levelup();
-        mathijs.levelup();
-        System.out.println(jael.getName() + " " + jael.getLevel() + " " + jael.getBaseAttributes());
-
     }
 }
--- test/RPG/CharacterTest.java
+++ test/RPG/CharacterTest.java
@@ -7,10 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterTest {
     Mage testMage;
+    Ranger testRanger;
+    Rogue testRogue;
+    Warrior testWarrior;
 
     @BeforeEach
     void setUp() {
-        testMage = new Mage("name");
+        testMage = new Mage("testMage");
+        testRanger = new Ranger("testRanger");
+        testRogue = new Rogue("testRogue");
+        testWarrior = new Warrior("testWarrior");
     }
 
     @Test
@@ -25,6 +31,34 @@ class CharacterTest {
         int expected = 2;
         testMage.levelup();
         int actual = testMage.getLevel();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void mageDefaultAttributes_validInputs_ShouldReturnOneOneEight() {
+        String expected = "1, 1, 8";
+        String actual = testMage.getBaseAttributes();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rangerDefaultAttributes_validInputs_ShouldReturnOneSevenOne() {
+        String expected = "1, 7, 1";
+        String actual = testRanger.getBaseAttributes();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rogueDefaultAttributes_validInputs_ShouldReturnTwoSixOne() {
+        String expected = "2, 6, 1";
+        String actual = testRogue.getBaseAttributes();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void warriorDefaultAttributes_validInputs_ShouldReturnFiveTwoOne() {
+        String expected = "5, 2, 1";
+        String actual= testWarrior.getBaseAttributes();
         assertEquals(expected, actual);
     }
 }
