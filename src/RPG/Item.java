a945794b2e470361bbdb73d9761e83fb4084d89f
savannah-borst <savannah.borst@gmail.com>
2022-02-14 18:06:37 +0100

    Initialise weapon class with constructor

--- src/RPG/Item.java
+++ src/RPG/Item.java
@@ -4,7 +4,6 @@ public abstract class Item {
     private String name;
     private int requiredLevel;
     private String slot;
-    //slot
 
     public Item(String name, int requiredLevel, String slot) {
         this.name = name;
---
+++ src/RPG/Weapon.java
@@ -0,0 +1,9 @@
+package RPG;
+
+public class Weapon extends Item {
+
+    //constructor
+    public Weapon(String name, int requiredLevel, String slot) {
+        super(name, requiredLevel, slot);
+    }
+}
