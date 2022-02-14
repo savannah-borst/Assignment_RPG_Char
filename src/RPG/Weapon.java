87afac70aaa123e04551aa202e4c514bb21679e4
savannah-borst <savannah.borst@gmail.com>
2022-02-14 18:07:01 +0100

    adding enum weapon with weapon types.

--- src/RPG/Weapon.java
+++ src/RPG/Weapon.java
@@ -2,6 +2,16 @@ package RPG;
 
 public class Weapon extends Item {
 
+    enum WeaponType {
+        AXES,
+        BOWS,
+        DAGGERS,
+        HAMMERS,
+        STAFFS,
+        SWORDS,
+        WANDS
+    }
+
     //constructor
     public Weapon(String name, int requiredLevel, String slot) {
         super(name, requiredLevel, slot);
