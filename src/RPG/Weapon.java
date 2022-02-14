23c31efd7b4ffc093741e450a54db69d183f0da9
savannah-borst <savannah.borst@gmail.com>
2022-02-14 20:16:18 +0100

    Adding enum in constructor testing in Main if it works

--- src/RPG/Weapon.java
+++ src/RPG/Weapon.java
@@ -1,7 +1,9 @@
 package RPG;
 
 public class Weapon extends Item {
+    WeaponType type;
 
+    //took some inspiration from: https://stackoverflow.com/questions/48627744/how-do-you-implement-an-enum-in-an-object-class-java/48627811
     enum WeaponType {
         AXES,
         BOWS,
@@ -13,7 +15,8 @@ public class Weapon extends Item {
     }
 
     //constructor
-    public Weapon(String name, int requiredLevel, String slot) {
+    public Weapon(String name, int requiredLevel, String slot, WeaponType type) {
         super(name, requiredLevel, slot);
+        this.type = type;
     }
 }
