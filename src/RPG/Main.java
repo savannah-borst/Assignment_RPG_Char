b17c3228db144c647e69af29437865b0cce9ec55
savannah-borst <savannah.borst@gmail.com>
2022-02-14 20:54:14 +0100

    emptied main + some unversioned class files

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -5,8 +5,5 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        Weapon Mjolnir = new Weapon("Mjolnir", 5, "head", Weapon.WeaponType.HAMMERS);
-
-        System.out.println(Mjolnir.type);
     }
 }
