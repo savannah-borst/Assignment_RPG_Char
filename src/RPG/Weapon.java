7d977357deb1ee8cb60171bcdd8c8e16797466f0
savannah-borst <savannah.borst@gmail.com>
2022-02-14 20:28:15 +0100

    adding other fields + passing through constructor + setting up getters for the fields

--- src/RPG/Weapon.java
+++ src/RPG/Weapon.java
@@ -2,8 +2,11 @@ package RPG;
 
 public class Weapon extends Item {
     WeaponType type;
+    private final int damage;
+    private final double attackPerSec;
+    private final double DPS;
 
-    //took some inspiration from: https://stackoverflow.com/questions/48627744/how-do-you-implement-an-enum-in-an-object-class-java/48627811
+        //took some inspiration from: https://stackoverflow.com/questions/48627744/how-do-you-implement-an-enum-in-an-object-class-java/48627811
     enum WeaponType {
         AXES,
         BOWS,
@@ -15,8 +18,29 @@ public class Weapon extends Item {
     }
 
     //constructor
-    public Weapon(String name, int requiredLevel, String slot, WeaponType type) {
+    public Weapon(String name, int requiredLevel, String slot, WeaponType type, int damage, double attackPerSec) {
         super(name, requiredLevel, slot);
         this.type = type;
+        this.damage = damage;
+        this.attackPerSec = attackPerSec;
+        this.DPS = damage * attackPerSec;
     }
+
+    public WeaponType getType() {
+        return type;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackPerSec() {
+        return attackPerSec;
+    }
+
+    public double getDPS() {
+        return DPS;
+    }
+
+
 }
