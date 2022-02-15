bafebc60b360c7acbc9bc381614b50f960843bfb
savannah-borst <savannah.borst@gmail.com>
2022-02-15 16:23:57 +0100

    testing in main + classes

--- src/RPG/Main.java
+++ src/RPG/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
         Warrior thor = new Warrior("Thor");
         Weapon mjolnir = new Weapon("Mjolnir", 1, Slot.WEAPON, Weapon.Type.HAMMER,5, 4.5);
-        thor.setEquipment(mjolnir);
+        thor.Equip(mjolnir);
 
         Armor chestPlate = new Armor("Chest plate", 1, Slot.BODY, Armor.Type.PLATE, 10, 2, 1);
-        thor.setEquipment(chestPlate);
+        thor.Equip(chestPlate);
 
         System.out.println(thor.getEquipment());
         //System.out.println(thor.getCharEquipment());
