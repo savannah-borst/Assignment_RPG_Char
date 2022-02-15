a249bdad1faf9a6ab1cdc9625e6a69f49e4c2e39
savannah-borst <savannah.borst@gmail.com>
2022-02-15 13:49:33 +0100

    got rid of the equipment references

--- src/RPG/Character.java
+++ src/RPG/Character.java
@@ -1,19 +1,16 @@
 package RPG;
 
 public abstract class Character {
-
     private final String name;
     private int level;
     private PrimaryAttribute basePrimaryAttributes;
     private PrimaryAttribute totalPrimaryAttributes;
-    private Equipment charEquipment;
 
     //constructor
     public Character(String name, int strength, int dexterity, int intelligence) {
         this.name = name;
         this.level = 1;
         this.basePrimaryAttributes = new PrimaryAttribute(strength, dexterity, intelligence); //set up PrimaryAttribute
-        this.charEquipment = new Equipment();
     }
 
     public String getName() {
@@ -36,14 +33,4 @@ public abstract class Character {
         return this.basePrimaryAttributes.getAllAttributes();
     }
 
-    public void equipWeapon(Weapon weapon) {
-        this.charEquipment.setWeapon(weapon);
-    }
-
-    //refactor to get weapon
-    public String getCharEquipment() {
-        return this.charEquipment.getWeapon();
-    }
-
-    //public abstract void equipArmor();
 }
