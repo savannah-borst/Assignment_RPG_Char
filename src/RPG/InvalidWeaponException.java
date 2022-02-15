fdad2ddd1c5b59f242345bfb30b60676ebd78c85
savannah-borst <savannah.borst@gmail.com>
2022-02-15 14:07:38 +0100

    adding Invalid weapon and armor exception files

---
+++ src/RPG/InvalidWeaponException.java
@@ -0,0 +1,9 @@
+package RPG;
+
+public class InvalidWeaponException extends Exception{
+
+    // credit: https://alvinalexander.com/java/java-custom-exception-create-throw-exception/
+    public InvalidWeaponException(String message) {
+        super(message);
+    }
+}
