7a5456d956fed583de871507f408c616e7bf3240
savannah-borst <savannah.borst@gmail.com>
2022-02-16 16:00:56 +0100

    removing unused getters  + fields and constructor setters

--- src/RPG/Weapon.java
+++ src/RPG/Weapon.java
@@ -1,8 +1,6 @@
 package RPG;
 
 public class Weapon extends Item {
-    private final int damage;
-    private final double attackPerSec;
     private final double DPS;
 
         //took some inspiration from: https://stackoverflow.com/questions/48627744/how-do-you-implement-an-enum-in-an-object-class-java/48627811
@@ -19,17 +17,7 @@ public class Weapon extends Item {
     //constructor
     public Weapon(String name, int requiredLevel, Slot slot, Type type, int damage, double attackPerSec) {
         super(name, requiredLevel, slot, type);
-        this.damage = damage;
-        this.attackPerSec = attackPerSec;
         this.DPS = damage * attackPerSec;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public double getAttackPerSec() {
-        return attackPerSec;
     }
 
     public double getDPS() {
