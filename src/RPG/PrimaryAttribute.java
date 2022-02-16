4fb516be742cdc2c1c739ba9b40ceef5e6a1b659
savannah-borst <savannah.borst@gmail.com>
2022-02-16 15:58:38 +0100

    removing unused setters

--- src/RPG/PrimaryAttribute.java
+++ src/RPG/PrimaryAttribute.java
@@ -20,24 +20,12 @@ public class PrimaryAttribute {
         return strength;
     }
 
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
     public int getDexterity() {
         return dexterity;
     }
 
-    public void setDexterity(int dexterity) {
-        this.dexterity = dexterity;
-    }
-
     public int getIntelligence() {
         return intelligence;
-    }
-
-    public void setIntelligence(int intelligence) {
-        this.intelligence = intelligence;
     }
 
     //Set all by adding the passed argument attributes
