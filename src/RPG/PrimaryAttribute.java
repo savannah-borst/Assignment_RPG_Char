a925d05415c7879817483b15cb6b0cd69b37e96f
savannah-borst <savannah.borst@gmail.com>
2022-02-16 09:48:00 +0100

    adding the general getters and  setters to PrimaryAttribute

--- src/RPG/PrimaryAttribute.java
+++ src/RPG/PrimaryAttribute.java
@@ -12,7 +12,31 @@ public class PrimaryAttribute {
         this.intelligence = intelligence;
     }
 
-    //Set all by adding the passed argumented attributes
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    //Set all by adding the passed argument attributes
     public void setAllAttributes(int strength, int dexterity, int intelligence) {
         this.strength += strength;
         this.dexterity += dexterity;
