747d9d78648c2f8584e14d1432f85676b29868a1
savannah-borst <savannah.borst@gmail.com>
2022-02-16 13:27:17 +0100

    setting up empty constructor in PrimAttr class  + edit set and get allAttr

--- src/RPG/PrimaryAttribute.java
+++ src/RPG/PrimaryAttribute.java
@@ -6,6 +6,10 @@ public class PrimaryAttribute {
     private int intelligence;
 
     //constructor
+    public PrimaryAttribute() {
+
+    }
+
     public PrimaryAttribute(int strength, int dexterity, int intelligence) {
         this.strength = strength;
         this.dexterity = dexterity;
@@ -38,13 +42,13 @@ public class PrimaryAttribute {
 
     //Set all by adding the passed argument attributes
     public void setAllAttributes(int strength, int dexterity, int intelligence) {
-        this.strength += strength;
-        this.dexterity += dexterity;
-        this.intelligence += intelligence;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
     }
 
     //getter get all attributes as string with , between
     public String getAllAttributes() {
-        return strength + ", " + dexterity + ", " + intelligence;
+        return strength + "," + dexterity + "," + intelligence;
     }
 }
